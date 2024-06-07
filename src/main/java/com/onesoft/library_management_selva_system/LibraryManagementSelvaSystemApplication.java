9ec8933f681a99cfa2ac18d3150c904527f322cf
SelvaKumarC3188 <selvak070@gmail.com>
2024-06-07 15:56:26 +0530

    Library

---
+++ src/main/java/com/onesoft/library_management_selva_system/LibraryManagementSelvaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.onesoft.library_management_selva_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSelvaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSelvaSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onesoft/library_management_selva_system/LibraryManagementSelvaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onesoft.library_management_selva_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSelvaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
